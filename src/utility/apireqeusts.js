c584e6316b512155909a2de27ee065221f6c3b9a
vipink28 <vipinkalyan28@gmail.com>
2024-04-18 10:04:38 +0530

    netflix api request

--- src/utility/apireqeusts.js
+++ src/utility/apireqeusts.js
@@ -1,7 +1,7 @@
 const API_KEY = '0d8ab7cff2692bd014bb25fca16d7158';
 
 const apirequests = {
-    getNetflixOriginals: `discover/tv?api_key=0d8ab7cff2692bd014bb25fca16d7158&language=en-US&page=1&with_networks=213`
+    getNetflixOriginals: `discover/tv?api_key=${API_KEY}&language=en-US&page=1&with_networks=213`
 }
 
 
